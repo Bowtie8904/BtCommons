b49b4853b8b5bf55518b7034e8e58c8c40f9b388
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-03-05 20:24:48 +0100

    Added randomID method which creates a random String with a configurable length.

--- src/bt/utils/id/StringID.java
+++ src/bt/utils/id/StringID.java
@@ -1,5 +1,11 @@
 package bt.utils.id;
 
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SplittableRandom;
 import java.util.UUID;
 
 /**
@@ -8,8 +14,77 @@ import java.util.UUID;
  */
 public final class StringID
 {
+    private static char[] availableChars;
+    private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+    private static final SplittableRandom random = new SplittableRandom();
+
+    /**
+     * Loads all letters from the given charset, so that they can be used by {@link #randomID(int)}.
+     * 
+     * <p>
+     * If this method is not called explicitly, it will be called with the argument {@link StandardCharsets#UTF_8}.
+     * </p>
+     * 
+     * @param charset
+     *            The charset whichs letters should be used for future random IDs.
+     */
+    public static void loadCharset(Charset charset)
+    {
+        CharsetEncoder enc = charset.newEncoder();
+        List<Character> characters = new ArrayList<Character>();
+
+        for (char c = 0; c < Character.MAX_VALUE; c ++ )
+        {
+            if (enc.canEncode(c) && Character.isLetter(c))
+            {
+                characters.add(c);
+            }
+        }
+
+        char[] tempArr = new char[characters.size()];
+
+        for (int i = 0; i < characters.size(); i ++ )
+        {
+            tempArr[i] = characters.get(i);
+        }
+
+        availableChars = tempArr;
+    }
+
+    /**
+     * Wrapper method of {@link UUID#randomUUID()} for consistency.
+     * 
+     * @return
+     */
     public static String uniqueID()
     {
         return UUID.randomUUID().toString();
+    }
+
+    /**
+     * Returns a random String with the given length.
+     * 
+     * <p>
+     * The used characters are defined by {@link #loadCharset(Charset)}.
+     * </p>
+     * 
+     * @param length
+     * @return
+     */
+    public static String randomID(int length)
+    {
+        if (availableChars == null)
+        {
+            loadCharset(DEFAULT_CHARSET);
+        }
+
+        var builder = new StringBuilder();
+
+        for (int i = 0; i < length; i ++ )
+        {
+            builder.append(availableChars[random.nextInt(availableChars.length)]);
+        }
+
+        return builder.toString();
     }
 }
