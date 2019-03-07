37c7cc959162c3ee17194bd8661b9b5e8a0a7db4
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-03-07 23:46:49 +0100

    Added create method and byte variations.

--- src/bt/utils/collections/array/Array.java
+++ src/bt/utils/collections/array/Array.java
@@ -38,6 +38,18 @@ public final class Array
      *            The elements for the array.
      * @return The array.
      */
+    public static byte[] of(byte... elements)
+    {
+        return elements;
+    }
+
+    /**
+     * Creates an array of the given elements.
+     * 
+     * @param elements
+     *            The elements for the array.
+     * @return The array.
+     */
     public static short[] of(short... elements)
     {
         return elements;
@@ -106,7 +118,7 @@ public final class Array
      */
     public static <T> T[] push(T[] arr, T element)
     {
-        T[] newArr = (T[])java.lang.reflect.Array.newInstance(element.getClass(), arr.length + 1);
+        T[] newArr = (T[])create(element.getClass(), arr.length + 1);
 
         for (int i = 0; i < arr.length; i ++ )
         {
@@ -116,5 +128,208 @@ public final class Array
         newArr[newArr.length - 1] = element;
 
         return newArr;
+    }
+
+    /**
+     * Pushes the given element into the array.
+     * 
+     * <p>
+     * The new element will be at the last index in the array after this operation.
+     * </p>
+     * 
+     * @param arr
+     *            The base array.
+     * @param element
+     *            The element to push into the array.
+     * @return The new array with a size of arr.length + 1.
+     */
+    public static boolean[] push(boolean[] arr, boolean element)
+    {
+        boolean[] newArr = new boolean[arr.length + 1];
+
+        for (int i = 0; i < arr.length; i ++ )
+        {
+            newArr[i] = arr[i];
+        }
+
+        newArr[newArr.length - 1] = element;
+
+        return newArr;
+    }
+
+    /**
+     * Pushes the given element into the array.
+     * 
+     * <p>
+     * The new element will be at the last index in the array after this operation.
+     * </p>
+     * 
+     * @param arr
+     *            The base array.
+     * @param element
+     *            The element to push into the array.
+     * @return The new array with a size of arr.length + 1.
+     */
+    public static byte[] push(byte[] arr, byte element)
+    {
+        byte[] newArr = new byte[arr.length + 1];
+
+        for (int i = 0; i < arr.length; i ++ )
+        {
+            newArr[i] = arr[i];
+        }
+
+        newArr[newArr.length - 1] = element;
+
+        return newArr;
+    }
+
+    /**
+     * Pushes the given element into the array.
+     * 
+     * <p>
+     * The new element will be at the last index in the array after this operation.
+     * </p>
+     * 
+     * @param arr
+     *            The base array.
+     * @param element
+     *            The element to push into the array.
+     * @return The new array with a size of arr.length + 1.
+     */
+    public static short[] push(short[] arr, short element)
+    {
+        short[] newArr = new short[arr.length + 1];
+
+        for (int i = 0; i < arr.length; i ++ )
+        {
+            newArr[i] = arr[i];
+        }
+
+        newArr[newArr.length - 1] = element;
+
+        return newArr;
+    }
+
+    /**
+     * Pushes the given element into the array.
+     * 
+     * <p>
+     * The new element will be at the last index in the array after this operation.
+     * </p>
+     * 
+     * @param arr
+     *            The base array.
+     * @param element
+     *            The element to push into the array.
+     * @return The new array with a size of arr.length + 1.
+     */
+    public static int[] push(int[] arr, int element)
+    {
+        int[] newArr = new int[arr.length + 1];
+
+        for (int i = 0; i < arr.length; i ++ )
+        {
+            newArr[i] = arr[i];
+        }
+
+        newArr[newArr.length - 1] = element;
+
+        return newArr;
+    }
+
+    /**
+     * Pushes the given element into the array.
+     * 
+     * <p>
+     * The new element will be at the last index in the array after this operation.
+     * </p>
+     * 
+     * @param arr
+     *            The base array.
+     * @param element
+     *            The element to push into the array.
+     * @return The new array with a size of arr.length + 1.
+     */
+    public static long[] push(long[] arr, long element)
+    {
+        long[] newArr = new long[arr.length + 1];
+
+        for (int i = 0; i < arr.length; i ++ )
+        {
+            newArr[i] = arr[i];
+        }
+
+        newArr[newArr.length - 1] = element;
+
+        return newArr;
+    }
+
+    /**
+     * Pushes the given element into the array.
+     * 
+     * <p>
+     * The new element will be at the last index in the array after this operation.
+     * </p>
+     * 
+     * @param arr
+     *            The base array.
+     * @param element
+     *            The element to push into the array.
+     * @return The new array with a size of arr.length + 1.
+     */
+    public static float[] push(float[] arr, float element)
+    {
+        float[] newArr = new float[arr.length + 1];
+
+        for (int i = 0; i < arr.length; i ++ )
+        {
+            newArr[i] = arr[i];
+        }
+
+        newArr[newArr.length - 1] = element;
+
+        return newArr;
+    }
+
+    /**
+     * Pushes the given element into the array.
+     * 
+     * <p>
+     * The new element will be at the last index in the array after this operation.
+     * </p>
+     * 
+     * @param arr
+     *            The base array.
+     * @param element
+     *            The element to push into the array.
+     * @return The new array with a size of arr.length + 1.
+     */
+    public static double[] push(double[] arr, double element)
+    {
+        double[] newArr = new double[arr.length + 1];
+
+        for (int i = 0; i < arr.length; i ++ )
+        {
+            newArr[i] = arr[i];
+        }
+
+        newArr[newArr.length - 1] = element;
+
+        return newArr;
+    }
+
+    /**
+     * Creates a new array of the given type with the given size.
+     * 
+     * @param type
+     *            The type of the array elements.
+     * @param size
+     *            The size of the returned array.
+     * @return The created array.
+     */
+    public static <T> T[] create(Class<T> type, int size)
+    {
+        return (T[])java.lang.reflect.Array.newInstance(type, size);
     }
 }
