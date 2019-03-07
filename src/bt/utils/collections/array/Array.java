75f0d32b0ab54a80f0911c3ec9b1dee3a0e0e282
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-03-07 22:40:06 +0100

    Created Array class with basic array creation methods.

---
+++ src/bt/utils/collections/array/Array.java
@@ -0,0 +1,93 @@
+package bt.utils.collections.array;
+
+/**
+ * Simple array utilities.
+ * 
+ * @author &#8904
+ */
+public class Array
+{
+    /**
+     * Creates an array of the given elements.
+     * 
+     * @param elements
+     *            The elements for the array.
+     * @return The array.
+     */
+    public <T> T[] of(T... elements)
+    {
+        return elements;
+    }
+
+    /**
+     * Creates an array of the given elements.
+     * 
+     * @param elements
+     *            The elements for the array.
+     * @return The array.
+     */
+    public boolean[] of(boolean... elements)
+    {
+        return elements;
+    }
+
+    /**
+     * Creates an array of the given elements.
+     * 
+     * @param elements
+     *            The elements for the array.
+     * @return The array.
+     */
+    public short[] of(short... elements)
+    {
+        return elements;
+    }
+
+    /**
+     * Creates an array of the given elements.
+     * 
+     * @param elements
+     *            The elements for the array.
+     * @return The array.
+     */
+    public int[] of(int... elements)
+    {
+        return elements;
+    }
+
+    /**
+     * Creates an array of the given elements.
+     * 
+     * @param elements
+     *            The elements for the array.
+     * @return The array.
+     */
+    public long[] of(long... elements)
+    {
+        return elements;
+    }
+
+    /**
+     * Creates an array of the given elements.
+     * 
+     * @param elements
+     *            The elements for the array.
+     * @return The array.
+     */
+    public float[] of(float... elements)
+    {
+        return elements;
+    }
+
+    /**
+     * Creates an array of the given elements.
+     * 
+     * @param elements
+     *            The elements for the array.
+     * @return The array.
+     */
+    public double[] of(double... elements)
+    {
+        return elements;
+    }
+}
