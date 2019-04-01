6202b680052a88d2b266c8aee3891f97d458febe
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-04-01 19:51:14 +0200

    Added a mutable integer wrapper.

---
+++ src/bt/types/MutableInt.java
@@ -0,0 +1,30 @@
+package bt.types;
+
+/**
+ * @author &#8904
+ *
+ */
+public class MutableInt
+{
+    private int i;
+
+    public MutableInt(int i)
+    {
+        this.i = i;
+    }
+
+    public void add(int j)
+    {
+        this.i += j;
+    }
+
+    public void set(int j)
+    {
+        this.i = j;
+    }
+
+    public int get()
+    {
+        return this.i;
+    }
+}
