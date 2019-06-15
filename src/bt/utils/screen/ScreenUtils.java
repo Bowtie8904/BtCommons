5390b592953e7860996d68075dadc3389694596a
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-06-15 14:03:56 +0200

    Added screen utils.

---
+++ src/bt/utils/screen/ScreenUtils.java
@@ -0,0 +1,49 @@
+package bt.utils.screen;
+
+import java.awt.Dimension;
+import java.awt.DisplayMode;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
+/**
+ * @author &#8904
+ *
+ */
+public final class ScreenUtils
+{
+    /**
+     * Gets the dimensions in pixels of the screen device with the given index.
+     * 
+     * @param screenIndex
+     * @return
+     */
+    public static Dimension getScreenDimensions(int screenIndex)
+    {
+        GraphicsDevice device = getScreens()[screenIndex];
+        DisplayMode mode = device.getDisplayMode();
+        return new Dimension(mode.getWidth(), mode.getHeight());
+    }
+
+    /**
+     * Gets the dimensions in pixels of the default screen device.
+     * 
+     * @return
+     */
+    public static Dimension getScreenDimensions()
+    {
+        GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        DisplayMode mode = device.getDisplayMode();
+        return new Dimension(mode.getWidth(), mode.getHeight());
+    }
+
+    /**
+     * Gets an array of all available screen devices.
+     * 
+     * @return
+     */
+    public static GraphicsDevice[] getScreens()
+    {
+        GraphicsEnvironment environment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        return environment.getScreenDevices();
+    }
+}
