43fe5da433b7662b8d556b3465e0b84d1aaef454
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-06-26 23:52:20 +0200

    Added InputStream json parse method.

--- src/bt/utils/json/JSON.java
+++ src/bt/utils/json/JSON.java
@@ -1,12 +1,15 @@
 package bt.utils.json;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import bt.utils.files.FileUtils;
+import bt.utils.log.Logger;
 
 /**
  * A utility class to perform JSON operations.
@@ -31,6 +34,43 @@ public final class JSON
 
         JSONTokener tokener = new JSONTokener(json);
         JSONObject object = null;
+
+        try
+        {
+            object = new JSONObject(tokener);
+        }
+        catch (JSONException e)
+        {
+            return null;
+        }
+        return object;
+    }
+
+    /**
+     * Parses the given JSON InpuitStream to a valid JSONObject.
+     * 
+     * @param json
+     *            The json InputStream to parse.
+     * @return The parsed JSONObject or null if the InputStream was null or incorrectly formatted.
+     */
+    public static JSONObject parse(InputStream json)
+    {
+        if (json == null)
+        {
+            return null;
+        }
+
+        JSONTokener tokener = new JSONTokener(json);
+        JSONObject object = null;
+
+        try
+        {
+            json.close();
+        }
+        catch (IOException e1)
+        {
+            Logger.global().print(e1);
+        }
 
         try
         {
