374b5b1c2271117e35111591e4a9a17bd465b1ed
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-08-31 00:22:01 +0200

    Added comment.

--- src/bt/utils/nulls/Null.java
+++ src/bt/utils/nulls/Null.java
@@ -6,6 +6,16 @@ package bt.utils.nulls;
  */
 public final class Null
 {
+    /**
+     * Checks if the first parameter is null and if so, returns the second parameter as default value.
+     *
+     * @param <T>
+     * @param checkValue
+     *            The value to be checked for null.
+     * @param defaultValue
+     *            The default value that is returned if the checkValue is null.
+     * @return Either the checkValue if it is not null or the defaultValue.
+     */
     public static <T> T nvl(T checkValue, T defaultValue)
     {
         return checkValue == null ? defaultValue : checkValue;
