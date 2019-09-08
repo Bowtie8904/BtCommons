b73f66892b66cecff2dc3ad6a4390c1eaaf7ba87
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-09-09 01:03:48 +0200

    Added conversion convinience methods.

--- src/bt/utils/num/NumberUtils.java
+++ src/bt/utils/num/NumberUtils.java
@@ -69,4 +69,55 @@ public final class NumberUtils
             return num;
         }
     }
+
+    public static long hexToDecimal(String hex)
+    {
+        return Long.parseLong(hex.trim(), 16);
+    }
+
+    public static String hexToOctal(String hex)
+    {
+        long dec = Long.parseLong(hex.trim(), 16);
+        return Long.toOctalString(dec).toUpperCase();
+    }
+
+    public static String hexToBinary(String hex)
+    {
+        long dec = Long.parseLong(hex.trim(), 16);
+        return Long.toBinaryString(dec).toUpperCase();
+    }
+
+    public static long binaryToDecimal(String binary)
+    {
+        return Long.parseLong(binary.trim(), 2);
+    }
+
+    public static String binaryToOctal(String binary)
+    {
+        long dec = Long.parseLong(binary.trim(), 2);
+        return Long.toOctalString(dec).toUpperCase();
+    }
+
+    public static String binaryToHex(String binary)
+    {
+        long dec = Long.parseLong(binary.trim(), 2);
+        return Long.toHexString(dec).toUpperCase();
+    }
+
+    public static long octalToDecimal(String octal)
+    {
+        return Long.parseLong(octal.trim(), 2);
+    }
+
+    public static String octalToBinary(String octal)
+    {
+        long dec = Long.parseLong(octal.trim(), 2);
+        return Long.toBinaryString(dec).toUpperCase();
+    }
+
+    public static String octalToHex(String octal)
+    {
+        long dec = Long.parseLong(octal.trim(), 2);
+        return Long.toHexString(dec).toUpperCase();
+    }
 }
