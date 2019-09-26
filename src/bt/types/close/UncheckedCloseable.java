328568a59bca59ce89a1bd941e1d28bbeda0aca2
Bowtie8904 <lukas.hartwig@vanderlande.com>
2019-09-26 09:13:04 +0200

    Added UncheckedClosable.

---
+++ src/bt/types/close/UncheckedCloseable.java
@@ -0,0 +1,26 @@
+package bt.types.close;
+
+/**
+ * @author &#8904
+ *
+ */
+public interface UncheckedCloseable extends Runnable, AutoCloseable
+{
+    @Override
+    default void run()
+    {
+        try
+        {
+            close();
+        }
+        catch (Exception ex)
+        {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    static UncheckedCloseable wrap(AutoCloseable c)
+    {
+        return c::close;
+    }
+}
