ef60e1b14d5b6ad0e55047316118985bc78b8b21
Bowtie8904 <lukas.hartwig@vanderlande.com>
2019-10-18 10:10:26 +0200

    Added copyable.

---
+++ src/bt/types/Copyable.java
@@ -0,0 +1,38 @@
+package bt.types;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import bt.utils.log.Logger;
+import bt.utils.refl.field.Fields;
+
+/**
+ * @author &#8904
+ *
+ */
+public interface Copyable<T>
+{
+    default public T copy()
+    {
+        T copy = null;
+        try
+        {
+            Constructor<T> construct = (Constructor<T>)getClass().getConstructor();
+            construct.setAccessible(true);
+            copy = construct.newInstance();
+
+            for (var field : Fields.getAllFields(getClass()))
+            {
+                field.setAccessible(true);
+                field.set(copy, field.get(this));
+            }
+        }
+        catch (InstantiationException | IllegalAccessException
+               | InvocationTargetException | SecurityException | NoSuchMethodException e1)
+        {
+            Logger.global().print(e1);
+        }
+
+        return copy;
+    }
+}
